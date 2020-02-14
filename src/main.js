4d248a10b28b250cf5314232db91a4076b9baa78
Nadeesha Dilruwan <eclayaz@gmail.com>
2020-02-15 01:02:04 +0300

    access state in component through getters

--- src/main.js
+++ src/main.js
@@ -5,5 +5,6 @@ import store from "./store/index";
 Vue.config.productionTip = false;
 
 new Vue({
+	store,
 	render: h => h(App)
 }).$mount("#app");
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -2,9 +2,22 @@
 
 import axios from "axios";
 
-const state = {};
+const state = {
+	todos: [
+		{
+			id: 1,
+			title: "Todo One"
+		},
+		{
+			id: 2,
+			title: "Todo Two"
+		}
+	]
+};
 
-const getters = {};
+const getters = {
+	allTodos: state => state.todos
+};
 
 const actions = {};
 
